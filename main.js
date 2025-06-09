6dc6088608b74e1e59c006226dd66649b743e291
fullstack9285 <fexrihuseynov085@gmail.com>
2025-06-09 08:35:11 -0700

    Salam

---
+++ main.js
@@ -0,0 +1,68 @@
+/* console.log("My name is Fakhri"); */
+
+/* let name = "Fakhri";
+console.log(name) */
+
+/* Bu adlardan hansının dəyişən üçün istifadə oluna biləcəyini göstərin. Birdən çox variant seçin.
+
+2user
+c@rt
+person  ==== bu
+title color
+console
+$add  ----bu */
+
+/* let bookprice = 10;
+let bookamount = 5;
+let totalprice = bookprice * bookamount;
+console.log( totalprice); */
+
+/* 
+let kmhSpeed = 90;
+let msSpeed = kmhSpeed * (1000 / 3600);
+console.log( msSpeed);
+*/
+
+//İstifadəçinin doğum tarixini soruşan və bu günə qədər yaşadığı günlərin sayını göstərən proqram yazın.
+
+/* 
+let birthDate = prompt("Doğum tarixini daxil edin ");
+let year = 2025-birthDate;
+let today = birthDate * 365;
+console.log(today)
+*/
+
+// Ədədlər üzərində əməliyyatları yerinə yetirin. num dəyişənində iki rəqəmli ədəd saxlanılır. Belə proqram yazın:
+
+// Bu ədədin beşinci dərəcə qüvvətə qaldırılmış vəziyyətini.
+// 3-ə bölünmədən qalan bu ədədin qalığını.
+
+
+// let num = 25;
+// let quvvet = num ** 5;
+// console.log(quvvet);
+
+
+/* 
+let firstName ="Fakhri";
+let lastName = "Huseynov";
+let fullName = "Sizin adiniz" + " " +  firstName + " " + lastName;
+console.log(fullName);
+*/
+
+/* 
+let fullName = "Fakhri Huseynov";
+console.log(fullName.length);
+*/
+
+/* 
+let fullName = "Fakhri Huseynov";
+let firstName = fullName.slice(0, 6);
+*/
+
+
+
+
+
+
+
